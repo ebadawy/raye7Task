00734939982bb7130692744eb8836b640019d44c
ehab albadawy <ehalbadawy93@gmail.com>
2016-09-04 00:15:02 +0200

    Add trip validations

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,2 +1,9 @@
 class Trip < ApplicationRecord
+	validates :driver, presence: true
+	validates :source, presence: true
+	validates :destination, presence: true
+	validates :time, presence: true
+	validates :seats,
+			  presence: true,
+			  numericality: true
 end
