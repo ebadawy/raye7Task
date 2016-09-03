0482d7a95d18a1df6b18d513fda3ac5b9fb34536
ehab albadawy <ehalbadawy93@gmail.com>
2016-09-04 00:05:29 +0200

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ApplicationRecord
+	validates :fname, presence: true
+	validates :lname, presence: true
+	validates :phone_number,
+			  uniqueness: true,
+			  presence: true,
+			  numericality: true,
+			  length: {minimum: 11, maximum: 14}
+	validate :validate_home_work_difference
+
+	def validate_home_work_difference
+		errors.add(:home_place, "can't be the same as work place") if home_place == work_place
+	end
 end
