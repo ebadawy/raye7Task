3a83a6aa9a24f3c958d125e3e11c24de4c24a23a
ehab albadawy <ehalbadawy93@gmail.com>
2016-09-04 03:40:02 +0200

    Add trip get/post methods

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,2 +1,23 @@
 class TripsController < ApplicationController
+	# GET /trips?user_id=X
+	def index
+		@trip = Trip.where(driver: params[:user_id])
+		render json: @trip
+	end
+
+	#POST /trips
+	def create
+		@trip = Trip.new(trip_params)
+
+	    if @trip.save
+			render json: @trip, status: :created, location: @trip
+	    else
+			render json: @trip.errors, status: :unprocessable_entity
+	    end
+	end
+
+	private
+	    def trip_params
+			params.require(:trip).permit(:driver_id, :source_id, :destination_id, :time, :seats)
+	    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
 	with_options only: [:index, :create] do |list_only|
 		list_only.resources :places
 		list_only.resources :groups
+		list_only.resources :trips
 	end
 end
